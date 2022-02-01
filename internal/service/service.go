168ea4fad34a9f57c4de312a4c837079a3717365
Jonathan Sun <jspsun@gmail.com>
2022-02-01 17:10:29 -0500

    initial

---
+++ internal/service/service.go
@@ -0,0 +1,16 @@
+package service
+
+import (
+	"context"
+	"github.com/OpenCal-FYDP/CalendarEventManagement/rpc"
+)
+
+type CalOptimizerService struct{}
+
+func (c *CalOptimizerService) OptimizeCal(ctx context.Context, req *rpc.EventReq) (*rpc.EventRes, error) {
+	panic("implement me")
+}
+
+func New() *CalOptimizerService {
+	return &CalOptimizerService{}
+}
